b98b8a619970825c12218c5068556c52f852132c
DanielaMara <danielamaradms@gmail.com>
2016-06-29 16:57:40 -0300

    Adicionando modelo ao exemplo simplemooc em Python

--- python/django/simplemooc/simplemooc/settings.py
+++ python/django/simplemooc/simplemooc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'simplemooc.core'
+    'simplemooc.core',
+    'simplemooc.courses',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
